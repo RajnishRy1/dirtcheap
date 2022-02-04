29395ddb7f4f3c13525442afd273c041ea4914a3
RajnishRy1 <virpaterajnish@gmail.com>
2022-02-04 13:43:02 +0530

    herokuErrors

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-if ('serviceWorker' in navigator) {
-  // declaring scope manually
-  navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function(error) {
-    console.log('Service worker registration failed:', error);
-  });
-} else {
-  console.log('Service workers are not supported.');
-}
+// if ('serviceWorker' in navigator) {
+//   // declaring scope manually
+//   navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
+//     console.log('Service worker registration succeeded:', registration);
+//   }, /*catch*/ function(error) {
+//     console.log('Service worker registration failed:', error);
+//   });
+// } else {
+//   console.log('Service workers are not supported.');
+// }
 
 
 
